697b312e6fba1a2ccbac7419ebb00dcfadb392a2
Muunatic <muunatic@typeslint.com>
2025-02-03 17:19:55 +0700

    feat: dynamically load command list from files

--- src/commands/interaction/help.ts
+++ src/commands/interaction/help.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction, EmbedBuilder } from '../../client';
 import { prefix } from '../../data/config';
@@ -7,15 +9,26 @@ module.exports = {
     .setName('help')
     .setDescription('Help command'),
     async execute(interaction: CommandInteraction) {
+        const commandsFolder = path.join(__dirname, '../../commands/message');
+        const commandFiles = fs.readdirSync(commandsFolder).filter((file) => file.endsWith('.js'));
+
+        const commands: string[] = [];
+
+        for (const file of commandFiles) {
+            const command = require(path.join(commandsFolder, file));
+            if (command.name) commands.push(command.name);
+        }
+
         const embed = new EmbedBuilder()
 
         .setColor('#89e0dc')
         .setTitle('Help commands')
+        .setThumbnail(interaction.client.user.avatarURL({extension: 'png', forceStatic: false, size: 4096}))
         .setDescription(`Prefix = **${prefix}**`)
-        .addFields({ name: 'General command', value: 'search, play, skip, stop, volume, repeat, pause, resume, queue, nowplaying, download' })
-        .setFooter({text: `Direquest oleh ${interaction.user.username}`, iconURL: interaction.user.avatarURL({extension: 'png', forceStatic: false, size: 1024})})
+        .addFields({ name: 'General command', value: commands.sort((a, b) => a.localeCompare(b)).join(', ') })
+        .setFooter({ text: `Direquest oleh ${interaction.user.username}`, iconURL: interaction.user.avatarURL({ extension: 'png', forceStatic: false, size: 1024 }) })
         .setTimestamp();
 
-        await interaction.reply({embeds: [embed]});
+        await interaction.reply({ embeds: [embed] });
     }
 };
